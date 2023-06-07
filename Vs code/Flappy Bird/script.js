122faf5a45bfb4c68ee13cdf323e682ed606a3c6
Isaac <isaacguedess10@gmail.com>
2023-06-07 20:33:25 -0300

    flb

--- Vs code/Flappy Bird/script.js
+++ Vs code/Flappy Bird/script.js
@@ -9,14 +9,14 @@ var ctx = canvas.getContext("2d");
 var bg = new Image();
 bg.src = "images/bg.png" 
 
- var chao = new Image();
- chao.scr = "images/chao.png"
+ var piso = new Image();
+ piso.scr = "images/piso.png"
 
  var canocima = new Image();
  canocima.src = "images/canocima.png "
 
  var canobaixo = new Image();
- canobaixo.src = "images/canocima.png"
+ canobaixo.src = "images/canobaixo.png"
 
  //variaveis  
 
@@ -39,7 +39,7 @@ bg.src = "images/bg.png"
  fly.scr = "sounds/fly.mp3"
 
  var score = new Audio();
- score.scr = "sounds/¨score.mp3"
+ score.scr = "sounds/score.mp3"
  
     //captura de tecla 
     document.addEventListener("keydown",voa);
@@ -53,6 +53,25 @@ bg.src = "images/bg.png"
      //fundo do jogo
      ctx.drawImage(bg,0,0)
      //DrawImage(imagens X, Y )
+    
+    //criando canos 
+
+    for(let i = 0; i < cano.length; i++){
+        //posição d cano de baixo
+        constant = canocima.height + eec  
+        //configurando o cano de cima 
+        ctx.drawImage(canocima, cano[i].x, cano[i].y)
+        //configurando o cano de baixo  
+        ctx.drawImage(canobaixo,cano[i].x, cano [i].y+constant)
+        //movimentaçao do cano 
+        cano [i].x = cano[i].x-1
+        if(cano[i].x == 125){
+          cano.push({
+            x : canvas.width,
+            y : Math.floor(Math.randon()*canocima.height)-canocima.height
+          })
+        }
+    }
 
      //criand canos 
      for( let i = 0 ; i< cano.length; i++){
@@ -62,7 +81,7 @@ bg.src = "images/bg.png"
 
      //desenhando o chão
 
-     ctx.drawImage(chao,0,canvas.height - chao.height)
+     ctx.drawImage(piso,0,canvas.height - piso.height)
 
      ctx.drawImage(bird,bX, bY)
      bY += gravity;
