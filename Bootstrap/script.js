bd7ec801320d2814f8777f1885ed56f127be4ba2
Isaac <isaacguedess10@gmail.com>
2023-06-20 19:44:56 -0300

    Bootstrap

---
+++ Bootstrap/script.js
@@ -0,0 +1,9 @@
+
+    var display = document.getElementById('display');
+ function changeText(){
+  display.innerHTML = 'o Botão foi pressionado'
+ }    
+ function selectText(text){
+  display.innerHTML = text;
+ }
+
